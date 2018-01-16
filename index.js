8233b1773fbc45634d9d61de5739d0f3395a3019
Harim Choi <harim1206@gmail.com>
2018-01-16 20:06:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.unshift(name)
+  return kittens
+
+  shift
+  unshift
+  push
+  pop
+  
+}
+function destructivelyPrependKitten(name){}
+function destructivelyRemoveLastKitten(name){}
+function destructivelyRemoveFirstKitten(name){}
