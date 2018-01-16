d62b26ef2d759317a0aca31438127aa0773ffeb1
Harim Choi <harim1206@gmail.com>
2018-01-16 20:10:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,3 +23,31 @@ function destructivelyRemoveFirstKitten(name){
   kittens.shift()
   return kittens
 }
+
+function appendKitten(name){
+  var newArr=[]
+  newArr = kittens.push(name)
+  return newArr
+  
+}
+
+function prependKitten(name){
+  var newArr=[]
+  newArr = kittens.unshift(name)
+  return newArr
+  
+}
+
+function removeLastKitten(){
+  var newArr=[]
+  newArr = kittens.pop()
+  return newArr
+  
+}
+
+function removeFirstKitten(){
+  var newArr=[]
+  newArr = kittens.shift()
+  return newArr
+  
+}
